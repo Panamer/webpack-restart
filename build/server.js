d813343a1696442e902b021778ca44807d74714f
guoyongwei <guoyongwei@gomefinance.com.cn>
2018-11-22 17:54:01 +0800

    1121晚上

---
+++ build/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+
+const config = require('./webpack.config.js')
+const app = express()
+const compiler = webpack(config)
+const port = process.env.PORT || 3000
+
+// Tell express to use the webpack-dev-middleware and use the webpack.config.js
+// configuration file as a base.
+app.use(webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath
+}))
+
+app.listen(port, () => {
+  console.log(`server listening on ${port}`);
+})
--- build/webpack.config.js
+++ build/webpack.config.js
@@ -7,11 +7,11 @@ module.exports = {
   entry: './src/index.js',
   devtool: 'inline-source-map',
   devServer:{
-    contentBase: '../dist'    // 以上配置告知 webpack-dev-server，在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件
+    contentBase: '../dist'                    // 以上配置告知 webpack-dev-server，在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件
   },
   plugins: [
     new CleanWebpackPlugin(['dist'], {
-      root: path.resolve(__dirname, "../"),  // 这个地方是../ 不是../dist  和output.path保持一致
+      root: path.resolve(__dirname, "../"),   // 这个地方是../ 不是../dist  和output.path保持一致
       verbose:  true
     }),
     new HtmlWebapckPlugin({
@@ -20,7 +20,8 @@ module.exports = {
   ],
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "../dist")
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/"                           // webpack-dev-middleware 需要配置这个 上面两个足够webpack-dev-server用了
   },
   module: {
     rules: [
