51aa31ee118c33dd28687e8b73cea3e9d06c9646
guoyongwei <guoyongwei@gomefinance.com.cn>
2018-11-27 18:03:45 +0800

    包很大

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import _ from 'lodash';
 import style from './style.css'
 import Icon from './icon.jpg'
 
@@ -6,8 +6,12 @@ function component() {
   var element = document.createElement('div');
    // Lodash, currently included via a script, is required for this line to work
    // Lodash, now imported by this script
-  element.innerHTML = 'wqewqewqrwrew';
+  element.innerHTML = _.join(['Hello,', 'webpack4.0'], '===');
   element.classList.add('hello')
+  // 将图片添加到div里
+  const image = new Image()
+  image.src = Icon
+  element.appendChild(image);
 
   return element;
 }
