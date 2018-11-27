01e97e024878fac7e99b86518596ed556831d15d
guoyongwei <guoyongwei@gomefinance.com.cn>
2018-11-27 18:01:34 +0800

    --hide-modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import _ from 'lodash';
+
 import style from './style.css'
 import Icon from './icon.jpg'
 
@@ -6,12 +6,8 @@ function component() {
   var element = document.createElement('div');
    // Lodash, currently included via a script, is required for this line to work
    // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello,', 'webpack4.0'], '666');
+  element.innerHTML = 'wqewqewqrwrew';
   element.classList.add('hello')
-  // 将图片添加到div里
-  const image = new Image()
-  image.src = Icon
-  element.appendChild(image);
 
   return element;
 }
