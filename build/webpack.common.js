4dd860e2ee94c284eb96f506d1a7ba7945522963
guoyongwei <guoyongwei@gomefinance.com.cn>
2018-11-28 13:37:35 +0800

    UglifyJSPlugin

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -20,6 +20,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+             presets: ['es2015']
+          }
+        }]
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       },
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,11 +1,14 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
+    // 使用这个插件需要安装上面的依赖包，另外安装babel-preset-es2015插件，找到 /\.js$/的rules，进行修改，根目录下添加【.babelrc】文件
+    // https://segmentfault.com/a/1190000011212544
     new UglifyJSPlugin({
         sourceMap: true //  避免在生产中使用 inline-*** 和 eval-***，因为它们可以增加 bundle 大小，并降低整体性能。
     }),
