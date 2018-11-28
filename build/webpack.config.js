e1fd83eea46a6b641a4b8b47aacf9a678672ac40
guoyongwei <guoyongwei@gomefinance.com.cn>
2018-11-28 18:07:48 +0800

    实现离线访问web app

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,21 +1,25 @@
 const path = require('path')
 const HtmlWebapckPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV ? process.env.NODE_ENV : 'development',
   entry: './src/index.js',
   devtool: 'inline-source-map',
-  devServer:{
-    contentBase: '../dist'    // 以上配置告知 webpack-dev-server，在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件
-  },
   plugins: [
     new CleanWebpackPlugin(['dist'], {
       root: path.resolve(__dirname, "../"),  // 这个地方是../ 不是../dist  和output.path保持一致
       verbose:  true
     }),
     new HtmlWebapckPlugin({
-      title: 'Output Management A'
+      title: '渐进式网络应用程序'
+    }),
+    new WorkboxPlugin.GenerateSW({
+       // 这些选项帮助 ServiceWorkers 快速启用
+       // 不允许遗留任何“旧的” ServiceWorkers
+       clientsClaim: true,
+       skipWaiting: true
     })
   ],
   output: {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import _ from 'lodash';
 import style from './style.css'
 import Icon from './icon.jpg'
 
+if ('serviceWorker' in navigator) {
+   window.addEventListener('load', () => {
+     navigator.serviceWorker.register('/service-worker.js').then(registration => {
+       console.log('SW registered: ', registration);
+     }).catch(registrationError => {
+       console.log('SW registration failed: ', registrationError);
+     });
+   });
+}
+
 function component() {
   var element = document.createElement('div');
    // Lodash, currently included via a script, is required for this line to work
