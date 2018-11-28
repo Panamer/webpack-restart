6bf517bc4b99ebbe155102915d1bc515b57104a5
guoyongwei <guoyongwei@gomefinance.com.cn>
2018-11-28 11:53:59 +0800

    区分生产和dev config

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -3,12 +3,7 @@ const HtmlWebapckPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: process.env.NODE_ENV ? process.env.NODE_ENV : 'development',
   entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer:{
-    contentBase: '../dist'    // 以上配置告知 webpack-dev-server，在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件
-  },
   plugins: [
     new CleanWebpackPlugin(['dist'], {
       root: path.resolve(__dirname, "../"),  // 这个地方是../ 不是../dist  和output.path保持一致
---
+++ build/webpack.dev.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer:{
+    contentBase: '../dist'    // 以上配置告知 webpack-dev-server，在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+       'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ]
+});
