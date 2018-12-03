ee08f8afe5b772618ec45fa0b7f9c4950f260f1f
guoyongwei <guoyongwei@gomefinance.com.cn>
2018-12-03 14:36:22 +0800

    配置eslint

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -6,6 +6,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer:{
+    port: 9090,
     contentBase: '../dist'    // 以上配置告知 webpack-dev-server，在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件
   },
   plugins: [
