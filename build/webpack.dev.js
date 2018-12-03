e20d5e7fee875a251ebc1ca8f526c61bd69bfbef
guoyongwei <guoyongwei@gomefinance.com.cn>
2018-12-03 16:54:05 +0800

    overlay: true 把eslint报错显示在页面上

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -7,6 +7,8 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer:{
     port: 9090,
+    quiet: true,
+    // overlay: true,  // 有eslint报错时把错误显示到页面上
     contentBase: '../dist'    // 以上配置告知 webpack-dev-server，在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件
   },
   plugins: [
